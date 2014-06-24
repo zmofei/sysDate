1b5a1e4f031e20b3c9871b90ccf0c2eaee9716e5
RobinMa <ahmzj@163.com>
2014-06-24 16:10:13 +0800

    Update sysDate.js
    modify code style

--- sysDate.js
+++ sysDate.js
@@ -1,22 +1,27 @@
-var sysDate = function() {
-    return sysDate['init'].apply(sysDate, arguments)
-};
+(function(root, factory) {
 
-(function(exp) {
-  exp.config={
-    url:'/'
+  var sysDate = function() {
+    return sysDate['init'].apply(sysDate, arguments)
   };
-  
+  factory(sysDate);
+
+  root.sysDate = sysDate;
+
+})(this, function(exp) {
+  exp.config = {
+    url: '/'
+  };
+
   exp.init = function() {
-    var arg=arguments[0];
-    for(i in arg){
-      exp.config[i]=arg[i]
+    var arg = arguments[0];
+    for (i in arg) {
+      exp.config[i] = arg[i]
     };
     exp.get();
     return exp;
   };
-  
-  exp.get = function(){
+
+  exp.get = function() {
     var xmlhttp;
 
     if (window.XMLHttpRequest) {
@@ -26,27 +31,26 @@ var sysDate = function() {
     }
     xmlhttp.onreadystatechange = function() {
       if (xmlhttp.readyState == 4) {
-        var sysD=xmlhttp.getResponseHeader('Date');
-        sysD=new Date(sysD);
-        sysD=Date.parse(sysD);
-        if(exp.doneFn){
+        var sysD = xmlhttp.getResponseHeader('Date');
+        sysD = new Date(sysD);
+        sysD = Date.parse(sysD);
+        if (exp.doneFn) {
           exp.doneFn(sysD);
         };
-        exp.config.lastDate=sysD;
+        exp.config.lastDate = sysD;
       }
     };
-    
-    exp.done = function(fn){
-      if(exp.config.lastDate){
-        fn&&fn(exp.config.lastDate);
-      }else{
-        exp.doneFn=fn;
+
+    exp.done = function(fn) {
+      if (exp.config.lastDate) {
+        fn && fn(exp.config.lastDate);
+      } else {
+        exp.doneFn = fn;
       }
-      
+
     }
-    
+
     xmlhttp.open("GET", exp.config.url, true);
     xmlhttp.send();
   }
-  
-})(sysDate);
+});
